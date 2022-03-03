2a290ac83da1aa86ddbbc9b95b76d7ee897f6adf
Visakh C <82100257+visakhc@users.noreply.github.com>
2022-03-04 02:41:40 +0530

    Changed Manifest intent filters

--- app/src/main/java/com/app/spendlog/SplashActivity.kt
+++ app/src/main/java/com/app/spendlog/SplashActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.app.spendlog.ui.HomeActivity
+import com.app.spendlog.ui.LoginActivity
 import com.app.spendlog.utils.SavedSession
 
 class SplashActivity : AppCompatActivity() {
@@ -16,7 +17,7 @@ class SplashActivity : AppCompatActivity() {
 
         if (userId.isEmpty() || userId == "notfound" || userId.isBlank()) {
             Log.d("ERROR", "Error---->SharedPreferences is empty")
-            val intent = Intent(this, HomeActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         } else {
